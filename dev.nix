32d655224c4c9da95bc19d673374a080fcf2902f
Satria Manggala Putra Pratama <satriampp@proton.me>
2026-05-18 12:32:46 +0700

    feat(dev): add many dev tools

--- dev.nix
+++ dev.nix
@@ -13,8 +13,24 @@
       vimAlias = true;
       defaultEditor = true;
     };
+
     lazygit = {
       enable = true;
+    };
+
+    git = {
+      enable = true;
+      lfs = {
+        enable = true;
+        enablePureSSHTransfer = true;
+      };
+    };
+
+    nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        gcc
+      ];
     };
   };
 
@@ -23,6 +39,7 @@
       enable = true;
       networkSocket.openFirewall = true;
     };
+
     docker = {
       enable = true;
     };
@@ -36,9 +53,26 @@
     gow
     python313
     tree-sitter
+    postgresql
     podman-tui
-    jre17_minimal
+    # jre17_minimal
     crun
     jetbrains.datagrip
+    rustup
+    rust-analyzer
+    lua
+    luarocks
+    luajitPackages.tree-sitter-cli
+    statix
+    nixfmt
+    openssl
+    gh
+    gcc
+    gh-dash
+    inputs.snitch.packages.${pkgs.stdenv.hostPlatform.system}.default
+    lazydocker
+    dockmate
+    sqlite
+    abduco
   ];
 }
