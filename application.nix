05f8b88a81b3ba433a976d9a990115c963aabdf2
Satria Manggala Putra Pratama <satriampp@proton.me>
2026-06-28 06:25:44 +0700

    feat(application): add AyuGram, some utility, and virtualization softwares

--- application.nix
+++ application.nix
@@ -5,11 +5,58 @@
 }:
 
 {
+  # nixpkgs.overlays = [
+  #   (self: super: {
+  #     qemu = super.qemu.override { venusSupport = true; };
+  #   })
+  # ];
+
   environment.systemPackages = with pkgs; [
     inputs.zen-browser.packages.${stdenv.hostPlatform.system}.default
+    # inputs.tgt.packages.${stdenv.hostPlatform.system}.default
+    ayugram-desktop
+    # librewolf
     zoom-us
     kdePackages.kamoso
+    gitlogue
     gnome-network-displays
+    weathr
     qbittorrent
+    nemu
+    virtiofsd
+    virt-viewer
   ];
+
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        vhostUserPackages = with pkgs; [ virtiofsd ];
+        runAsRoot = false;
+        swtpm = {
+          enable = true;
+        };
+        # package = pkgs.qemu.override {
+        #   venusSupport = true;
+        # };
+      };
+    };
+
+    kvmgt.enable = true;
+    # useEFIBoot = true;
+    # tpm.enable = true;
+    # useSecureBoot = true;
+  };
+
+  programs = {
+    virt-manager = {
+      enable = true;
+    };
+  };
+
+  # services = {
+  #   qbittorrent = {
+  #     enable = true;
+  #   };
+  # };
 }
