377ee9382ef8f103cdafd5de64fc24bd67214d40
Satria Manggala Putra Pratama <satriampp@proton.me>
2026-07-08 22:41:49 +0700

    feat(game): replace lutris with lutris-free, add esync, fix audio cracking

--- game.nix
+++ game.nix
@@ -9,7 +9,25 @@
     "vm.max_map_count" = 2147483642;
   };
 
-  security.rtkit.enable = true;
+  systemd.settings.Manager = {
+    DefaultLimitNOFILE = 524288;
+  };
+  security.pam.loginLimits = [
+    {
+      domain = "*";
+      type = "soft";
+      item = "nofile";
+      value = "524288";
+    }
+    {
+      domain = "*";
+      type = "hard";
+      item = "nofile";
+      value = "524288";
+    }
+  ];
+
+  # security.rtkit.enable = true;
   hardware.graphics.enable32Bit = true;
 
   # programs.steam.enable = true;
@@ -25,51 +43,36 @@
           96000
           176400
           192000
+          352800
+          384000
         ];
-        "default.clock.quantum" = 64;
-        "default.clock.min-quantum" = 64;
-        "default.clock.max-quantum" = 64;
+        # "default.clock.quantum" = 64;
+        # "default.clock.min-quantum" = 64;
+        # "default.clock.max-quantum" = 64;
         "stream.properties" = {
           "resample.quality" = 10;
         };
         "default.resample.method" = "speex-float-10";
       };
-      "context.modules" = [
-        {
-          name = "libpipewire-module-rt";
-          args = {
-            "nice.level" = -11;
-            "rt.prio" = 5;
-          };
-          flags = [
-            "ifexists"
-            "nofail"
-          ];
-        }
-      ];
+      # "context.modules" = [
+      #   {
+      #     name = "libpipewire-module-rt";
+      #     args = {
+      #       "nice.level" = -11;
+      #       "rt.prio" = 5;
+      #     };
+      #     flags = [
+      #       "ifexists"
+      #       "nofail"
+      #     ];
+      #   }
+      # ];
     };
   };
 
   environment.systemPackages = with pkgs; [
-    (lutris.override {
-      extraLibraries = pkgs: [ ];
-      extraPkgs = pkgs: [
-        gamemode
-        protontricks
-        winetricks
-        wineWow64Packages.full
 
-        openal-soft
-
-        mangohud
-        mangojuice
-
-        gamescope
-        umu-launcher
-        # inputs.prismlauncher.packages.${pkgs.system}.prismlauncher
-      ];
-    })
-
+    lutris-free
     gamemode
     protontricks
     winetricks
