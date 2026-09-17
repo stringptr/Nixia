12e5788002a48c0fb7f6d3100d0c813465126137
Satria Manggala Putra Pratama <satriampp@proton.me>
2026-09-17 13:20:48 +0700

    feat(dev): remove AzureStorageExplorer

--- azure.nix
+++ azure.nix
@@ -9,33 +9,45 @@
     azure-cli
   ];
 
-  services.gnome.gnome-keyring.enable = true;
-  security.polkit.enable = true;
+  # services.gnome.gnome-keyring.enable = true;
+  # security.polkit.enable = true;
 
-  programs = {
-    nix-ld = {
-      libraries = with pkgs; [
-        nspr
-        nss
-        atk
-        cups
-        dbus
-        cairo
-        gtk3
-        pango
-        libxcomposite
-        libxdamage
-        libxfixes
-        libxrandr
-        libxkbcommon
-        libgbm
-        expat
-        alsa-lib
-
-        gnome-keyring
-        polkit_gnome
-        libsecret
-      ];
-    };
-  };
+  # programs = {
+  #   nix-ld = {
+  #     enable = true;
+  #     libraries = with pkgs; [
+  #       nspr
+  #       nss
+  #       atk
+  #       cups
+  #       dbus
+  #       cairo
+  #       gtk3
+  #       pango
+  #       libxcomposite
+  #       libxdamage
+  #       libxfixes
+  #       libxrandr
+  #       libxkbcommon
+  #       libgbm
+  #       expat
+  #       alsa-lib
+  #
+  #       gnome-keyring
+  #       polkit_gnome
+  #       libsecret
+  #
+  #       # dotnetCorePackages.runtime_10_0-bin
+  #       # dotnetCorePackages.runtime_11_0-bin
+  #       # dotnetCorePackages.dotnet_10.runtime
+  #       # dotnetCorePackages.dotnet_11.runtime
+  #       # dotnet-runtime_10
+  #       # dotnet-runtime_11
+  #       # dotnet-sdk_10
+  #       # dotnet-sdk_11
+  #       # dotnet-aspnetcore_10
+  #       # dotnet-aspnetcore_11
+  #     ];
+  #   };
+  # };
 }
